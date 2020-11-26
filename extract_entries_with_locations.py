0e9adb4c60b9791836378e9f7e7b9243d2ea93f7
Davinaaa <778901472@qq.com>
2020-11-26 11:49:23 +0000

    add a path

--- extract_entries_with_locations.py
+++ extract_entries_with_locations.py
@@ -4,7 +4,8 @@ import os
 
 
 def read_file():
-    f = open(r"F:\Edin\1_1\Data Science for Design\Encyclopedia Britannica\Data\Raw\nls-text-encyclopaediaBritannica\193819047.txt", "r", encoding='UTF-8')
+    # f = open(r"F:\Edin\1_1\Data Science for Design\Encyclopedia Britannica\Data\Raw\nls-text-encyclopaediaBritannica\193819047.txt", "r", encoding='UTF-8')
+    f = open("/Users/wanfangdu/Desktop/DS4D/Project/nls-text-encyclopaediaBritannica/194167545.txt", "r")
     a = f.read()
     f.close()
     return a
