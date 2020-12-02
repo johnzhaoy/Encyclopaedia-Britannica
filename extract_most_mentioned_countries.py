c3add434c20f0eba964941fcec0e3f31684ad7f6
John Chao <johnzhaoy@yahoo.com>
2020-12-02 23:42:57 +0800

    added the most-mentioned countries in each continent in each edition

--- extract_most_mentioned_countries.py
+++ extract_most_mentioned_countries.py
@@ -1,65 +1,67 @@
 import heapq
 import os
 import pandas as pd
+import pycountry
 
 # path = r'/Users/wanfangdu/Desktop/DS4D/projectgit/extracted/edition'
 path = r'F:\Edin\1_1\Data Science for Design\Encyclopedia Britannica\Coding\Encyclopaedia-Britannica\extracted\edition'
 output_dir = r'F:\Edin\1_1\Data Science for Design\Encyclopedia Britannica\Coding\Encyclopaedia-Britannica\continent-related content\most_mentioned_countries'
-edition_list = []
-continent_list = []
-country_list = []
-dic = {'Edition': edition_list, 'Continent': continent_list, 'Country': country_list}
-df = pd.DataFrame(dic)
 
-def find_max_explain(file, edition_num, continent):
+
+def find_most_mentioned_countries(file, edition_num, continent):
     data = pd.read_csv(file)
-    # print(data.head())
-    explain = data['Entry'].tolist()
-    print(explain)
-    if len(explain) != 0:
-        longest = heapq.nlargest(1, explain, key=len)
-        edition_list.append(edition_num)
-        continent_list.append(continent)
-        country_list.append(longest[0])
+    country_code = data['Country code'].tolist()
+    for i in range(len(country_code)):
+        country_code[i] = str(country_code[i]).replace(',', ' ')
+    countries = " ".join(str(i) for i in country_code)
+    country_list = countries.split(" ")
 
-        # print("longest start")
-        # print(type(longest))
-        # longest = ','.join(longest)
-        # print(longest)
-        # print("longest done")
+    different_country_dictionary_list = []
+    different_country = []
 
-        # for x in longest:
-        #     print(type(x))
-        #     print(x)
-        # longest = max(explan, key=len)
-        # print(longest)# this is to find the longest one, use min() to find the shortest one
+    for k in range(len(country_list)):
+        if country_list[k] not in different_country:
+            different_country.append(country_list[k])
+            country_dictionary = {'Country': country_list[k], 'Frequency': 1}
+            different_country_dictionary_list.append(country_dictionary)
+        else:
+            for j in range(len(different_country)):
+                if different_country_dictionary_list[j]['Country'] == country_list[k]:
+                    different_country_dictionary_list[j]['Frequency'] = different_country_dictionary_list[j]['Frequency'] + 1
+
+    for i in range(len(different_country_dictionary_list)):
+        country_code = different_country_dictionary_list[i]['Country']
+        try:
+            different_country_dictionary_list[i]['Country'] = pycountry.countries.get(alpha_2=country_code).name
+        except:
+            continue
+
+    name = ['Country', 'Frequency']
+    df = pd.DataFrame(columns=name, data=different_country_dictionary_list)  # 数据有三列，列名分别为one,two,three
+    print(df)
+
+    edition = "edition"+edition_num
+    dirName = output_dir + "\\" + edition
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+    dirName = dirName + "\\" + continent
+    df.to_csv(dirName,index=False)
+
 
 
 def read_file(edition_num):
     edition_num = str(edition_num)
     dirs = os.listdir(path + edition_num)
     for file in dirs:
-        fp1 = path + edition_num + '/' + file
         if file != 'output.csv':
+            fp1 = path + edition_num + '/' + file
             f = open(fp1, encoding='utf-8')
             print()
             print('filename: ', file)
             a = f.read()
             f.close()
-            find_max_explain(fp1, edition_num, file)
-    return a
+            find_most_mentioned_countries(fp1, edition_num, file)
+
 
 for i in range(1,9):
     read_file(i)
-
-df['Edition'] = edition_list
-df['Continent'] = continent_list
-df['Entry'] = country_list
-
-
-# dirName = output_dir
-# if not os.path.exists(dirName):
-#     os.makedirs(dirName)
-# dirName = dirName + "\\"
-
-# df.to_csv(dirName + "longest.csv", index=False)
